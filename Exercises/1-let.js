b4d42430340c4707b230f8959d0601ea4432ec88
Elena Kononchuk <eva.kononchuk@gmail.com>
2022-03-27 18:33:00 +0300

    add 1, 2, 3 tasks and package-lock.json

--- Exercises/1-let.js
+++ Exercises/1-let.js
@@ -1,5 +1,5 @@
 'use strict';
 
-let name = undefined;
+let name = 'Elena';
 
 module.exports = { name };
--- Exercises/3-hello.js
+++ Exercises/3-hello.js
@@ -1,5 +1,5 @@
 'use strict';
 
-const hello = null;
+const hello = name => 'Hello' + hello('Elena');
 
 module.exports = { hello };
